221ef694a038f2ac3f6edaedef4b832f4d6df3e0
Agustin586 <marcelo123z.com@gmail.com>
2024-09-15 19:15:21 -0300

    Creacion Nodo 3 QP

---
+++ Nodos Can/Nodo 3/Quantum Leaps/Nodo3_QP/source/Nodo3_QP.h
@@ -0,0 +1,13 @@
+/**
+ * @file Nodo 3 con Quantum Leaps.
+ * @brief BSP para QPC.
+ * @author Zuliani, Agustin
+ * @date 15/09/24
+ */
+
+#ifndef NODO3_QP_H_
+#define NODO3_QP_H_
+
+
+
+#endif /* NODO3_QP_H_ */
